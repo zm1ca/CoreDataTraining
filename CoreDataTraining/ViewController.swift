2371388f4d52bdad12127556d2baf8748ca168f2
zm1ca <zmicier.fied@yandex.ru>
2021-01-08 21:50:35 +0300

    Initial Commit

---
+++ CoreDataTraining/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreDataTraining
+//
+//  Created by Źmicier Fiedčanka on 8.01.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
