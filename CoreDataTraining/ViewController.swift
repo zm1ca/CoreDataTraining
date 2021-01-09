b2e6bce48f6dfdd41ecd6d17d5540f1dee8949eb
zm1ca <zmicier.fied@yandex.ru>
2021-01-09 12:00:22 +0300

    stylising UI

--- CoreDataTraining/ViewController.swift
+++ CoreDataTraining/ViewController.swift
@@ -11,9 +11,25 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBackground
+        
+        navigationItem.title = "Companies"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(image: #imageLiteral(resourceName: "plus").withRenderingMode(.alwaysOriginal), style: .plain, target: self, action: #selector(handleAddCompany))
+        
+        setupNavigationStyle()
     }
-
-
+    
+    
+    func setupNavigationStyle() {
+        navigationController?.navigationBar.isTranslucent = false
+        //let lightRed = UIColor(red: 247/255, green: 66/255, blue: 82/255, alpha: 1)
+        //navigationController?.navigationBar.tintColor = lightRed
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationController?.navigationBar.largeTitleTextAttributes = [NSAttributedString.Key.foregroundColor:UIColor.white]
+    }
+    
+    @objc func handleAddCompany() {
+        print("adding")
+    }
 }
 
