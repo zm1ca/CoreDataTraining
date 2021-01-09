d6a2bee3c6fd6042d86630a818cd88af10421fbb
zm1ca <zmicier.fied@yandex.ru>
2021-01-09 12:00:22 +0300

    stylising UI

---
+++ CoreDataTraining/Supporting Files/CustiomNavigationController.swift
@@ -0,0 +1,14 @@
+//
+//  CustiomNavigationController.swift
+//  CoreDataTraining
+//
+//  Created by Źmicier Fiedčanka on 8.01.21.
+//
+
+import UIKit
+
+class CustiomNavigationController: UINavigationController {
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+}
