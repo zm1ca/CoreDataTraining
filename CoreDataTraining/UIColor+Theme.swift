935b542b20d721e0290d012f6cd88d5f6fe111be
zm1ca <zmicier.fied@yandex.ru>
2021-01-10 13:08:06 +0300

    stylized tableView

---
+++ CoreDataTraining/UIColor+Theme.swift
@@ -0,0 +1,16 @@
+//
+//  UIColor+Theme.swift
+//  CoreDataTraining
+//
+//  Created by Źmicier Fiedčanka on 10.01.21.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    static let CDTTealColor = UIColor(red: 48/255, green: 164/255, blue: 182/255, alpha: 1)
+    static let CDTLightRed = UIColor(red: 247/255, green: 66/255, blue: 82/255, alpha: 1)
+    static let CDTDarkBlue = UIColor(red: 9/255, green: 45/255, blue: 64/255, alpha: 1)
+    static let CDTLightBlue = UIColor(red: 218/255, green: 235/255, blue: 243/255, alpha: 1)
+}
