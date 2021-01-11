f34c442185436b37d254b0476aac4c195168152c
zm1ca <zmicier.fied@yandex.ru>
2021-01-11 10:39:27 +0300

    Adding singleton -- CoreDataManager

---
+++ CoreDataTraining/CoreDataManager.swift
@@ -0,0 +1,23 @@
+//
+//  CoreDataManager.swift
+//  CoreDataTraining
+//
+//  Created by Źmicier Fiedčanka on 11.01.21.
+//
+
+import CoreData
+
+class CoreDataManager {
+    
+    static let shared = CoreDataManager()
+    
+    let persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "CoreDataTrainingModels")
+        container.loadPersistentStores { (storeDescription, error) in
+            if let error = error {
+                fatalError("Loading of store failed: \(error)")
+            }
+        }
+        return container
+    }()
+}
--- CoreDataTraining/CreateCompanyController.swift
+++ CoreDataTraining/CreateCompanyController.swift
@@ -50,31 +50,19 @@ class CreateCompanyController: UIViewController {
     
     @objc private func handleSave() {
         
-        //Initialization of Core Data Stack
-        let persistantContainer = NSPersistentContainer(name: "CoreDataTrainingModels")
-        persistantContainer.loadPersistentStores { (storeDescription, error) in
-            if let error = error {
-                fatalError("Loading of store failed: \(error)")
-            }
-        }
-        
-        let context = persistantContainer.viewContext
+        let context = CoreDataManager.shared.persistentContainer.viewContext
         let company = NSEntityDescription.insertNewObject(forEntityName: "Company", into: context)
         company.setValue(nameTextField.text, forKey: "name")
         
-        //perform save
         do {
             try context.save()
+            
+            dismiss(animated: true) {
+                self.delegate?.addCompany(company: company as! Company)
+            }
+            
         } catch let saveError {
             print("Failed to save company:", saveError)
-        }
-        
-        
-        dismiss(animated: true) {
-//            guard let companyName = self.nameTextField.text else { return }
-//            
-//            let companyToSave = Company(name: companyName, founded: Date())
-//            self.delegate?.addCompany(company: companyToSave)
         }
     }
     
